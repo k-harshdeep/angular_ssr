7a3770f009470af429198c4fa2ebe370af0af405
Harshdeep Kaur <harshdeep@oasys.co>
2025-07-24 17:46:48 +0530

    Initial commit

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+ name = 'SSR Laptop';
+  price = 59999;
+  description = 'A high-performance laptop rendered on the server!';
+}
